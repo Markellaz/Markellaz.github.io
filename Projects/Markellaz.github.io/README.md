ab09bd03d95ec27d9a326b0ced9485a30326579b
Антон <Markovantony763@yandex.ru>
2024-04-30 14:40:19 +0400

    добавил ямл

---
+++ Projects/Markellaz.github.io/README.md
@@ -0,0 +1,1 @@
+# Markellaz.github.io
---
+++ Projects/Markellaz.github.io/js.js
@@ -0,0 +1,6 @@
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', function() {
+	alert('Катай на чем есть, скилл  есть скилл')
+})
